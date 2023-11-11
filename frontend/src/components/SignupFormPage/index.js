619913e8e58d43d6ba67e2751afc82775aa03810
Harrison Liang <liangharrison123@gmail.com>
2023-11-10 20:46:19 -0500

    auth fully working, including date

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -23,20 +23,37 @@ function SignupFormPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    // Check if all birthday components are selected
+    if (!birthday.month || !birthday.day || !birthday.year) {
+      setErrors(["Please select a valid birthday."]);
+      return;
+    }
+
     try {
       setErrors([]);
-      await dispatch(sessionActions.signup({ email, first_name, last_name, gender, password, birthday }));
+
+      // Format the birthday before dispatching
+      const formattedBirthday = `${birthday.year}-${birthday.month}-${birthday.day}`;
+
+      await dispatch(sessionActions.signup({
+        email,
+        first_name,
+        last_name,
+        gender,
+        password,
+        birthday: formattedBirthday,
+      }));
+
       // Handle successful signup if needed
     } catch (res) {
       let data;
       try {
-        // .clone() essentially allows you to read the response body twice
         data = await res.clone().json();
       } catch {
-        data = await res.text(); // Will hit this case if, e.g., server is down
+        data = await res.text();
       }
-  
+
       if (data?.errors) {
         setErrors(data.errors);
       } else if (data) {
@@ -44,8 +61,18 @@ function SignupFormPage() {
       } else {
         setErrors([res.statusText]);
       }
-    };
+    }
   };
+
+  // Generate an array of month names
+  const monthOptions = Array.from({ length: 12 }, (_, index) => (index + 1).toString().padStart(2, '0'));
+
+  // Generate an array of day numbers from 1 to 31
+  const dayOptions = Array.from({ length: 31 }, (_, index) => (index + 1).toString().padStart(2, '0'));
+
+  // Generate an array of years, adjust the range as needed
+  const currentYear = new Date().getFullYear();
+  const yearOptions = Array.from({ length: 100 }, (_, index) => (currentYear - index).toString());
 
   return (
     <>
@@ -92,39 +119,45 @@ function SignupFormPage() {
         </label>
         <label>
           Birthday
-            <select value={birthday.month} onChange={(e) => setBirthday({ ...birthday, month: e.target.value })}>
-                <option value="01">Month</option>
-                {/* Add the rest of the month options */}
-            </select>
-            <select value={birthday.day} onChange={(e) => setBirthday({ ...birthday, day: e.target.value })}>
-                <option value="19">Day</option>
-                {/* Add the rest of the day options */}
-            </select>
-            <select value={birthday.year} onChange={(e) => setBirthday({ ...birthday, year: e.target.value })}>
-                <option value="1990">Year</option>
-                {/* Add the rest of the year options */}
-            </select>
+          <select value={birthday.month} onChange={(e) => setBirthday({ ...birthday, month: e.target.value })}>
+            <option value="">Month</option>
+            {monthOptions.map((month) => (
+              <option key={month} value={month}>{month}</option>
+            ))}
+          </select>
+          <select value={birthday.day} onChange={(e) => setBirthday({ ...birthday, day: e.target.value })}>
+            <option value="">Day</option>
+            {dayOptions.map((day) => (
+              <option key={day} value={day}>{day}</option>
+            ))}
+          </select>
+          <select value={birthday.year} onChange={(e) => setBirthday({ ...birthday, year: e.target.value })}>
+            <option value="">Year</option>
+            {yearOptions.map((year) => (
+              <option key={year} value={year}>{year}</option>
+            ))}
+          </select>
         </label>
         <label>
           Gender
-            <label>
-                <input
-                    type="radio"
-                    value="male"
-                    checked={gender === "male"}
-                    onChange={() => setGender("male")}
-                />
-                Male
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    value="female"
-                    checked={gender === "female"}
-                    onChange={() => setGender("female")}
-                />
-                Female
-            </label>
+          <label>
+            <input
+              type="radio"
+              value="male"
+              checked={gender === "male"}
+              onChange={() => setGender("male")}
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="female"
+              checked={gender === "female"}
+              onChange={() => setGender("female")}
+            />
+            Female
+          </label>
         </label>
         <button type="submit">Sign Up</button>
       </form>
