210a9a7668c78660558efe164ab27d4fc4a3ac6f
Harrison Liang <liangharrison123@gmail.com>
2023-11-16 15:13:05 -0500

    overall structure for profile page

---
+++ frontend/src/components/Friends/FriendsList.js
@@ -0,0 +1,10 @@
+const FriendsList = () => {
+    return (
+        <>
+            <h3>Friends List will go here</h3>
+        </>
+    )
+}
+
+export default FriendsList;
+
--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser, fetchUser } from '../../store/users';
+import FriendsList from '../Friends/FriendsList';
+import MakePosts from '../Posts/MakePosts';
+import AllPosts from '../Posts/AllPosts';
+import "./ProfilePage.css";
 
 const ProfilePage = () => {
     const dispatch = useDispatch();
@@ -13,13 +17,32 @@ const ProfilePage = () => {
     }, [userId]);
 
     return (
-        <>
-            <h1>Profile Page</h1>
-            <h1>{user?.id}</h1>
-            <h1>{user?.firstName}</h1>
-            <h1>{user?.lastName}</h1>
+        <div id="profile-page">
+            <div id="cover-photo">
+                <p>Cover photo will be here</p>
+            </div>
+            <div id="profile-info-container">
+                <div id="profile-pic">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+                <div id="profile-info">
+                    <h1>{`${user?.firstName} ${user?.lastName}`}</h1>
+                </div>
+            </div>
+            <div id="profile-container">
+                <div id="profile-left">
+                    <h3>Bio will be here</h3>
+                    <FriendsList />
+                </div>
+                <div id="profile-right">
+                    <MakePosts />
+                    <AllPosts />
+                </div>
+            </div>
+            <div class="clearfix"></div>
+
             <Link to="/">Back</Link>
-        </>
+        </div>
     )
 }
 
