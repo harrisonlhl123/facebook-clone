7b038003880ac63ed59d6dbc46699eaa4e2aac7b
Harrison Liang <liangharrison123@gmail.com>
2023-12-16 14:58:48 -0500

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_13_160535) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_16_195727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,7 +77,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_13_160535) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "feed_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["feed_id"], name: "index_posts_on_feed_id"
   end
 
   create_table "users", force: :cascade do |t|
