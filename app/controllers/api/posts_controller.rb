36be6cbe42c67c854e499c62217dec059951b4e8
Harrison Liang <liangharrison123@gmail.com>
2023-12-16 15:07:42 -0500

    controller done

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -41,6 +41,6 @@ class Api::PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:body, :author_id)
+        params.require(:post).permit(:body, :author_id, :feed_id)
     end
 end
