afcba9f1cb4a561b6b8ddcb467e462202532a6c9
Harrison Liang <liangharrison123@gmail.com>
2023-12-21 14:52:15 -0500

    working on reducers

--- frontend/src/store/friends.js
+++ frontend/src/store/friends.js
@@ -1,6 +1,19 @@
 import csrfFetch from "./csrf.js";
 import { receiveUsers } from "./users.js"
 
+const CREATE_FRIEND = 'friends/CREATE_FRIEND';
+const DESTROY_FRIEND = 'friends/DESTROY_FRIEND';
+
+export const createFriend = friend => ({
+    type: CREATE_FRIEND,
+    friend,
+});
+  
+export const destroyFriend = friendId => ({
+    type: DESTROY_FRIEND,
+    friendId,
+});
+
 export const getFriends = state => state.users ? Object.values(state.users) : [];
 
 export const fetchFriends = () => async (dispatch) => {
